e99222380d2eb320df16bbfb3dc3463d89bd3b24
Aleks0378 <aleksstepan0378@gmail.com>
2023-09-27 12:35:36 +0300

    Добавьте файлы проекта.

---
+++ Task_1/Journal.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class Journal
+    {
+        public string name;
+        public int year;
+        public string comment;
+        public string phone;
+        public string email;
+        public int count;
+        public Journal() { }
+        public Journal(string name, int year, string comment, string phone, string email, int count)
+        {
+            this.name = name;
+            this.year = year;
+            this.comment = comment;
+            this.phone = phone;
+            this.email = email;
+            this.count = count;
+        }
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value == " " || value.Length == 0)
+                {
+                    throw new Exception("Ошибка: Пустая строка, или пробел!");
+                }
+                name = value;
+            }
+        }
+        public int Year { get { return year; } set { year = value; } }
+        public string Comment
+        {
+            get { return comment; }
+            set
+            {
+                if (value == " " || value.Length == 0)
+                {
+                    throw new Exception("Ошибка: Пустая строка, или пробел!");
+                }
+                comment = value;
+            }
+        }
+        public string Phone
+        {
+            get { return phone; }
+            set
+            {
+                if (value == " " || value.Length == 0)
+                {
+                    throw new Exception("Ошибка: Пустая строка, или пробел!");
+                }
+                phone = value;
+            }
+        }
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                if (value == " " || value.Length == 0)
+                {
+                    throw new Exception("Ошибка: Пустая строка, или пробел!");
+                }
+                email = value;
+            }
+        }
+        public int Count
+        {
+            get { return count; }
+            set { count = value; }
+        }
+        public override string ToString()
+        {
+            return $"\nНазвание журнала: {name}\tГод выпуска: {year}\t" +
+                $"Описание журнала: {comment}\tКонтактный телефон: {phone}\tКонтактный e-mail: {email}\tКолличество сотрудников: {count}\n";
+        }
+        public void Input()
+        {
+            Console.WriteLine("Введите название журнала: ");
+            Name = Console.ReadLine();
+            Console.WriteLine("Введите год выпуска журнала: ");
+            Year = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите описание журнала: ");
+            Comment = Console.ReadLine();
+            Console.WriteLine("Введите контактный телефон журнала: ");
+            Phone = Console.ReadLine();
+            Console.WriteLine("Введите контактный e-mail журнала: ");
+            Email = Console.ReadLine();
+            Console.WriteLine("Введите колличество сотрудников журнала: ");
+            Count = Convert.ToInt32(Console.ReadLine());
+        }
+
+
+        //перегрузка + (для увеличения количества сотрудников на указанную величину)
+        public static Journal operator +(Journal obj, int a)
+        {
+            obj.count += a;
+            return obj;
+        }
+
+        //перегрузка - (для уменьшения количества сотрудников на указанную величину)
+        public static Journal operator -(Journal obj, int a)
+        {
+            if (obj.count < a)
+            {
+                throw new Exception("Ошибка!!! Кол-во сотрудников < указанное число");
+            }
+            obj.count -= a;
+            return obj;
+        }
+
+        //перегрузка ++
+        public static Journal operator ++(Journal obj)
+        {
+            obj.count++;
+            return obj;
+        }
+
+        //перегрузка == (проверка на равенство количества сотрудников)
+        public static bool operator ==(Journal a, Journal b)
+        {
+            return a.count == b.count;
+        }
+        public static bool operator !=(Journal a, Journal b)
+        {
+            return a.count != b.count;
+        }
+
+        //перегрузка < и > (проверка на меньше или больше количества сотрудников)
+        public static bool operator <(Journal a, Journal b)
+        {
+            return a.count < b.count;
+        }
+        public static bool operator >(Journal a, Journal b)
+        {
+            return a.count > b.count;
+        }
+
+        //перегрузка Equals
+        public override bool Equals(object obj)
+        {
+            if (obj is Journal)
+            {
+                Journal j = obj as Journal;
+                if (this.name == j.name && this.year == j.year && this.comment == j.comment
+                    && this.phone == j.phone && this.email == j.email && this.count == j.count)
+                    return true;
+                else
+                    return false;
+            }
+            throw new Exception("Тип != Journal");
+        }
+    }
+}
---
+++ Task_1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region использование класса Журнал
+
+            Journal jour1 = new Journal("Forbes", 2010, "Bysiness and its life", "+15453245456", "forbes@gmail.com", 20);
+            Journal jour2 = new Journal("Forbes", 2010, "Bysiness and its life", "+15453245456", "forbes@gmail.com", 20);
+            Journal jour3 = new Journal("Day of the week", 2010, "Buildings and all about", "+15455678845", "dayoftheweek@gmail.com", 20);
+            Console.WriteLine(jour1);
+            Console.WriteLine(jour2);
+            Console.WriteLine(jour3);
+            try
+            {
+                //увеличение количества сотрудников на указанную величину
+                jour3 = jour3 + 5;
+                Console.WriteLine($"\nДобавили сотрудников: {jour3}");
+                // уменьшение количества сотрудников на указанную величину
+                jour3 = jour3 - 20;
+                Console.WriteLine($"\nУбрали сотрудников: {jour3}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            // операции: ==, !=, <, >
+            if (jour1 == jour3)
+                Console.WriteLine("Кол-во сотрудников объектов Равно");
+            if (jour1 != jour3)
+                Console.WriteLine("Кол-во сотрудников объектов НЕ равно");
+            if (jour1 > jour3)
+                Console.WriteLine($"Кол-во сотрудников журнала {jour1.Name} Больше кол-ва сотрудников журнала {jour3.Name}");
+            else if (jour1 < jour3)
+                Console.WriteLine($"Кол-во сотрудников журнала {jour1.Name} Меньше кол-ва сотрудников журнала {jour3.Name}");
+            else
+                Console.WriteLine("Кол-во сотрудников обеих журналов Равно");
+            try
+            {
+                if (jour1.Equals(jour2))
+                    Console.WriteLine("Объекты Равны");
+                else
+                    Console.WriteLine("Объекты НЕ равны");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            #endregion
+        }
+    }
+}
